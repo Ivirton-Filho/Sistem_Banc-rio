adf1743cbd3f504238ca9c1797a621d8330d926c
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-07-07 12:32:35 -0300

    ADD função de sair e simplificação do código

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #declarações de variáveis que serão utilizadas no código
 acao_do_user = 0
-extrato = [" "]
+extrato = []
 saldo = 0
 deposito = 0
 saque = 0
 controle = ""
 i = 0
-
 
 #tela de boas-vindas
 print("#############################################")
@@ -18,10 +17,7 @@ while True:
     print("Oque você deseja fazer?")
     acao_do_user = int(input(" 1-Deposito\n 2-Saque\n 3-Extrato\n 4-Sair\n Escolha a opção conrespondente: "))
 
-    if acao_do_user < 1 or acao_do_user >= 5:
-        print("\nPorfavor, digite uma alternativa valida.")
-    else :
-        match acao_do_user:
+    match acao_do_user:
             case 1:
                 while True:
                     print("\nOpção de depósito foi selecionada!\n")
@@ -29,7 +25,7 @@ while True:
 
                     #lógica para que a operação de depositivo ocorra
                     saldo =+ float(deposito)
-                    extrato.append("R$ +" + deposito)
+                    extrato.append("+ R$" + deposito)
                     print("\nO valor foi depositado!\n")
 
                     # estrutura que controla se o user deseja realizar a operação novamente
@@ -51,14 +47,16 @@ while True:
                         print("Operação inválida!\nLimite máximo de R$500 por saque.")
                         # zerar o valor do saque para evitar repetições desnecessárias
                         saque = 0
+
                     elif saque > saldo:
                         print("Operação inválida!\nSaldo não disponível.")
                         #zerar o valor do saque para evitar repetições desnecessárias
                         saque = 0
+                        
                     else:
                         saldo = saldo - saque
                         saque = str(saque)
-                        extrato.append("R$ -" + saque)
+                        extrato.append("- R$" + saque)
                         print("\nO valor foi retirado!\n")
 
                         #estrutura que controla se o user deseja realizar a operação novamente
@@ -66,11 +64,11 @@ while True:
                         controle = controle.upper()
                         if controle == "S":
                             continue
-                            controle = ""
+                            controle = " "
 
                         else:
                             break
-                            controle = ""
+                            controle = " "
                 #mensagem caso o user tente fazer mais de 3 tentativas de saque
                 if i == 3:
                     print("\nLimite diario de 3 tentativas ultrapassado.")
@@ -82,3 +80,9 @@ while True:
                 for uso_conta in extrato:
                     print(uso_conta)
                     print(f"O saldo atual da conta é: R$ {saldo}")
+            case 4:
+                print("\nObrigado por utilizar nossos serviços! :)\n")
+                break
+
+            case default:
+                print("\nSelecione uma opção válida!\n")
